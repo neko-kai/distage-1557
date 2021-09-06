e70a83563ddd576888e0d2e9175d1be89e348e73
Kai <kai@7mind.io>
2021-09-06 12:25:09 +0100

    Fix #1557: Close (non-daemon) execution context by putting under DI lifecycle instead of global

--- src/main/scala/distage1557/Launcher.scala
+++ src/main/scala/distage1557/Launcher.scala
@@ -4,23 +4,17 @@ import cats.effect.ContextShift
 import cats.effect.IO
 import distage.plugins.PluginDef
 import fs2.kafka.{KafkaProducer, ProducerSettings, Serializer}
-import izumi.distage.model.definition.ModuleDef
+import izumi.distage.model.definition.{Lifecycle, ModuleDef}
 import izumi.distage.plugins.PluginConfig
 import izumi.distage.roles.RoleAppMain.LauncherCats
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 import java.util.concurrent.Executors
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
 object Launcher extends LauncherCats[IO] {
-
-  private implicit val ec: ExecutionContextExecutor =
-    ExecutionContext.fromExecutor(
-      Executors.newFixedThreadPool(10)
-    )
-
-  private implicit val contextShiftIO: ContextShift[IO] = IO.contextShift(ec)
 
   override protected def pluginConfig: PluginConfig =
     PluginConfig.const(
@@ -35,12 +29,14 @@ object Launcher extends LauncherCats[IO] {
     include(
       new ModuleDef {
         make[KafkaProducer[IO, String, String]].fromResource {
-          KafkaProducer.resource(
-            ProducerSettings(
-              keySerializer = Serializer.string[IO],
-              valueSerializer = Serializer.string[IO]
-            ).withBootstrapServers("localhost:9092")
-          )
+          implicit c: ContextShift[IO] =>
+            KafkaProducer.resource(
+              ProducerSettings(
+                keySerializer = Serializer.string[IO],
+                valueSerializer = Serializer.string[IO]
+              ).withBootstrapServers("localhost:9092")
+                .withCloseTimeout(1.second)
+            )
         }
 
         make[DummyService[IO]].fromResource(
@@ -59,7 +55,12 @@ object Launcher extends LauncherCats[IO] {
             Slf4jLogger.fromName[IO]("stuck-role")
           )
 
-        make[ExecutionContext].from(ec)
+        make[ExecutionContext].fromResource {
+          Lifecycle
+            .fromExecutorService(Executors.newFixedThreadPool(10))
+            .map(ExecutionContext.fromExecutor(_))
+        }
+        make[ContextShift[IO]].from(IO.contextShift _)
       }
     )
   }
